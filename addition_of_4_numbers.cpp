18083e1b1e292651bd13d0674052e4c3985cde89
chougale-sarvadnya <sarvadnyachougale@gmail.com>
2024-12-23 11:17:19 +0530

    Add files via upload

---
+++ addition_of_4_numbers.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,c,d,sum;
+cout<<"enter first value=";
+cin>>a;
+cout<<"enter second value=";
+cin>>b;
+cout<<"enter third value=";
+cin>>c;
+cout<<"enter fourth value=";
+cin>>d;
+sum=a+b+c+d;
+cout<<"addition of 4 values="<<sum;
+return 0;
+}
