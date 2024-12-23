5509b26939230d4a83acd59d78fa7acb60b2a275
chougale-sarvadnya <sarvadnyachougale@gmail.com>
2024-12-23 11:17:52 +0530

    Add files via upload

---
+++ addn_of_2num.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a,b,sum;
+cout<<"enter a and b:"<<endl;
+cin>>a>>b;
+sum=a+b;
+cout<<"addition of 2 numbers is :"<<sum<<endl;
+return 0;
+}
